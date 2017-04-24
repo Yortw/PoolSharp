86becd2674479ea8f494989a01d05a22ad3ebe8f
Nicolas Gonzalez <nicolasgonzalez@MacBook-Pro-de-Nicolas.local>
2017-04-24 12:02:11 -0300

    Migrate to netstandard1.6

--- src/BuildNugetPackage.cmd
+++
@@ -1,4 +0,0 @@
-del /F /Q /S *.CodeAnalysisLog.xml
-
-"..\.nuget\NuGet.exe" pack -sym PoolSharp.nuspec -BasePath .\
-pause
---
+++ src/PoolSharp/PoolSharp.Shared/ReflectionUtils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Linq;
+
+namespace PoolSharp
+{
+    internal static class ReflectionUtils
+    {
+        internal static bool IsTypeWrapped(Type type)
+        {
+            return type.GetTypeInfo().IsGenericType && (type.GetGenericTypeDefinition() == typeof(PooledObject<>));
+
+        }
+
+        internal static bool IsTypeDisposable(Type type, bool isTypeWrapped)
+        {
+            if (isTypeWrapped)
+                type = type.GetTypeInfo().GenericTypeArguments.First();
+
+            return (from i in type.GetTypeInfo().ImplementedInterfaces where i == typeof(IDisposable) select i).Any();
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal static bool Contains<T>(this T[] items, T item)
+        {
+            if (items == null) return false;
+
+            for (int cnt = 0; cnt < items.Length; cnt++)
+            {
+                if (EqualityComparer<T>.Default.Equals(items[cnt], item))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
